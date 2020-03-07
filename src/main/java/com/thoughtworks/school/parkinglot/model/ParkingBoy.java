7bef7e47483d7059150a6b6ff1a0c1fa08f67320
kbyyd24 <melo@gaoyuexiang.cn>
2020-03-07 21:28:05 +0800

    remove test and implement of ParkingBoy, which won't need these methods

--- src/main/java/com/thoughtworks/school/parkinglot/model/ParkingBoy.java
+++ src/main/java/com/thoughtworks/school/parkinglot/model/ParkingBoy.java
@@ -1,8 +1,6 @@
 package com.thoughtworks.school.parkinglot.model;
 
 import com.thoughtworks.school.parkinglot.annotation.Entity;
-import com.thoughtworks.school.parkinglot.exception.NoAvailableParkingLotException;
-import com.thoughtworks.school.parkinglot.exception.ParkingLotNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,29 +8,4 @@ import java.util.List;
 public class ParkingBoy {
 
   private List<ParkingLot> parkingLots = new ArrayList<>();
-
-  public void addNextParkingLot(ParkingLot parkingLot) {
-    parkingLots.add(parkingLot);
-  }
-
-  public void removeParkingLot(ParkingLot parkingLot) {
-    parkingLots.removeIf(parkingLot::equals);
-  }
-
-  public Receipt park(Car car) {
-    return parkingLots.stream()
-        .filter(ParkingLot::isAvailable)
-        .findFirst()
-        .map(parkingLot -> parkingLot.park(car))
-        .orElseThrow(NoAvailableParkingLotException::new);
-  }
-
-  public Car pickUp(Receipt receipt) {
-    ParkingLot relatedParkingLot =
-        parkingLots.stream()
-            .filter(parkingLot -> parkingLot.getId().equals(receipt.getParkingLotId()))
-            .findFirst()
-            .orElseThrow(ParkingLotNotFoundException::new);
-    return relatedParkingLot.pickUp(receipt);
-  }
 }
