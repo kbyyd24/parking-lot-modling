dac6753babf1180fe90247ad3018712923b96291
kbyyd24 <melo@gaoyuexiang.cn>
2020-03-07 22:22:22 +0800

    implement ParkingBoy.findOneParkingLot

--- src/main/java/com/thoughtworks/school/parkinglot/model/ParkingBoy.java
+++ src/main/java/com/thoughtworks/school/parkinglot/model/ParkingBoy.java
@@ -1,11 +1,20 @@
 package com.thoughtworks.school.parkinglot.model;
 
 import com.thoughtworks.school.parkinglot.annotation.Entity;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class ParkingBoy {
 
-  private List<ParkingLot> parkingLots = new ArrayList<>();
+  private List<ParkingLot> parkingLots;
+  private ParkingStrategy strategy;
+
+  public ParkingBoy(List<ParkingLot> parkingLots, ParkingStrategy strategy) {
+    this.parkingLots = parkingLots;
+    this.strategy = strategy;
+  }
+
+  public ParkingLot findOneParkingLot() {
+    return strategy.apply(parkingLots);
+  }
 }
--- src/test/groovy/com/thoughtworks/school/parkinglot/model/ParkingBoyTest.groovy
+++ src/test/groovy/com/thoughtworks/school/parkinglot/model/ParkingBoyTest.groovy
@@ -4,5 +4,14 @@ package com.thoughtworks.school.parkinglot.model
 import spock.lang.Specification
 
 class ParkingBoyTest extends Specification {
+  def "should return parking lot selected by strategy"(ParkingStrategy strategy, List<ParkingLot> parkingLots, ParkingLot expectedParkingLot) {
+    expect:
+    def parkingBoy = new ParkingBoy(parkingLots, strategy)
+    parkingBoy.findOneParkingLot() == expectedParkingLot
 
+    where:
+    strategy                                              | parkingLots                              | expectedParkingLot
+    new FirstAvailableStrategy()                          | [0, 1].collect { new ParkingLot(it) }    | parkingLots.get(1)
+    { lots -> lots.reverse().find({ it.isAvailable() }) } | [1, 2, 0].collect { new ParkingLot(it) } | parkingLots.get(1)
+  }
 }
