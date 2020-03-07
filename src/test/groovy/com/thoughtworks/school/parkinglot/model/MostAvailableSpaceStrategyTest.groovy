50fa032ecb40d8f85d81cf2ab5d9e46da66b6d66
kbyyd24 <melo@gaoyuexiang.cn>
2020-03-07 22:40:27 +0800

    return null when not find parking lot by MostAvailableSpaceStrategy

--- src/test/groovy/com/thoughtworks/school/parkinglot/model/MostAvailableSpaceStrategyTest.groovy
+++ src/test/groovy/com/thoughtworks/school/parkinglot/model/MostAvailableSpaceStrategyTest.groovy
@@ -1,5 +1,6 @@
 package com.thoughtworks.school.parkinglot.model
 
+
 import spock.lang.Specification
 
 class MostAvailableSpaceStrategyTest extends Specification {
@@ -14,5 +15,13 @@ class MostAvailableSpaceStrategyTest extends Specification {
     [1, 0, 2].collect { new ParkingLot(it) } | parkingLots.get(2)
     [0, 2, 1].collect { new ParkingLot(it) } | parkingLots.get(1)
     [3, 2, 1].collect { new ParkingLot(it) } | parkingLots.get(0)
+  }
+
+  def "should return null when not found most available space parking lot"() {
+    when:
+    def parkingLot = strategy.apply([])
+
+    then:
+    parkingLot == null
   }
 }
